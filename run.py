c21d65b007ac880846d2f96c5963209c756afb8f
Harsh Bhatt <hbhatt687@gmail.com>
2018-04-15 01:39:34 -0400

    commenting

--- run.py
+++ run.py
@@ -2,6 +2,13 @@
 # -*- coding: utf-8 -*-
 # pylint: disable=C0103,C0111
 
+# @author: Harsh Bhatt
+# The main purpose of this file is to make it easier to run the
+# program checking the results of the algorithm. One launch
+# of this file will run the algorthm real time on a GUI.
+# Much of the work done on this research topic was done in the
+# algorithms portion of the code.
+
 import argparse
 
 from snake.game import Game, GameConf, GameMode
